48b4f8e7f38b012ed5f06a69a55db9fe483b21ba
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-26 14:04:32 +0000

    can now also recognise the suffix ed

--- WildCards.java
+++ WildCards.java
@@ -10,12 +10,27 @@ class WildCards
 {
 	static String normalise(String wildcard)
 	{
-		int len;		//length of wildcard
-		len=wildcard.length();
+		int len=wildcard.length();	//length of wildcard
 
-		if(wildcard.substring(len-2,len).equals("ed"))
+		if(bi(wildcard))
 			return wildcard.substring(0,len-2);
 		else
 			return wildcard;
+	}
+
+	static boolean bi(String term)	//check if term has two letter suffix
+	{
+		int i;				//for for loops
+		int len=term.length();		//length of wildcard
+		String[] suffixes={"ed","er"};	//all know 2 letter suffixes
+
+		for(i=0; i<suffixes.length; i++)
+		{
+			if(term.substring(len-2,len).equals(suffixes[i]))
+				return true;
+		}
+
+		return false;
+
 	}
 }
