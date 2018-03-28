296d24e8f8fa8d4625f23d321079a999d68e1ef4
Liam Mc Cormick <ljmc2000@gmail.com>
2018-03-28 12:03:04 +0100

    added librarys for sql to spiderbot

--- Spiderbot.java
+++ Spiderbot.java
@@ -1,8 +1,18 @@
 //search through the library and make a record of which file contains which words
 
 package com.assignment1.search;
+
 import java.io.File;
 import java.nio.file.Paths;
+
+//sql stuff
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.*;
 
 class Spiderbot
 {
